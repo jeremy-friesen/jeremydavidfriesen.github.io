a3748685fc9b463282ce4724a7e29c8f1122fedc
jeremydavidfriesen <43259835+jeremydavidfriesen@users.noreply.github.com>
2019-01-29 23:01:29 -0500

    added navbar

---
+++ navbar.js
@@ -0,0 +1,13 @@
+window.onscroll = function(){addRemoveSticky()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function addRemoveSticky(){
+  if(window.pageYOffset >= sticky){
+    navbar.classList.add("sticky");
+  }else{
+    navbar.classList.remove("sticky");
+  }
+}
